aa270b0191bdca92f679f34f8c7602cc9206a12f
Rishitha <rishithapaladugu9@gmail.com>
2024-07-20 22:31:44 +0530

    first commit

---
+++ Lecture 33/REST_API_Express/server.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const mangoose=require('mongoose');
+const bodyParser=require('bodyParser');
+const routes=require('./routes/routes');
+
+const db_url='mongoose+srv://new-user-123@testcluster.vyftd.mongodb.net/<dbname>?retryWrites=true&w=majority'
+mongoose.connect(db_url, {useNewUrlParser: true},()=>{
+    console.log('*******Connection established to DB*******');
+})
+
+const app=express();
+
+//middleware of body parser
+app.use(bodyParser.json());
+
+//Form: /api/avengers
+app.use('/api', routes)
+
+//error handler middleware
+app.use((err, req,res,next)=>{
+   res.status(422).send({error: err.message});
+})
+
+app.listen(3001, ()=>{
+    console.log('Listening at port number 3001');
+})
